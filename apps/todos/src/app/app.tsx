adf665256ccaa6d901a8426a61ca0ef7a70097df
Mabel Loh <2269565+zgal@users.noreply.github.com>
2021-05-04 23:51:53 +0800

    React Nx Tutorial 7

--- apps/todos/src/app/app.tsx
+++ apps/todos/src/app/app.tsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
-interface Todo {
-  title: string;
-}
+import { Todo } from '@myorg/data';
 
 const App = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
