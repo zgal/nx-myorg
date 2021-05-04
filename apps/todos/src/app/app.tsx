ecccb2dcab22d247c5a3ccd8e43fe33688037206
Mabel Loh <2269565+zgal@users.noreply.github.com>
2021-05-04 22:40:44 +0800

    React Nx Tutorial 4

--- apps/todos/src/app/app.tsx
+++ apps/todos/src/app/app.tsx
@@ -1,22 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface Todo {
   title: string;
 }
 
-export const App = () => {
-  const [todos, setTodos] = useState<Todo[]>([
-    { title: 'Todo 1' },
-    { title: 'Todo 2' },
-  ]);
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    fetch('/api/todos')
+      .then((_) => _.json())
+      .then(setTodos);
+  }, []);
 
   function addTodo() {
-    setTodos([
-      ...todos,
-      {
-        title: `New todo ${Math.floor(Math.random() * 1000)}`,
-      },
-    ]);
+    fetch('/api/addTodo', {
+      method: 'POST',
+      body: '',
+    })
+      .then((_) => _.json())
+      .then((newTodo) => {
+        setTodos([...todos, newTodo]);
+      });
   }
 
   return (
